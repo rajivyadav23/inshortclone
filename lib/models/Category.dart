5f8e808347b48899a538fc87a8a9e3e6cc3118d8
Rajeev <rajeevyadav6@icloud.com>
2022-03-14 09:49:19 +0530

    models added

---
+++ lib/models/Category.dart
@@ -0,0 +1,21 @@
+class Category {
+  final String icon, title;
+
+  const Category({required this.icon, required this.title});
+}
+
+const List<Category> demo_categories = [
+  Category(icon: "#", title: "All"),
+  Category(icon: "#", title: "National"),
+  Category(icon: "#", title: "Business"),
+  Category(icon: "#", title: "Sports"),
+  Category(icon: "#", title: "World"),
+  Category(icon: "#", title: "Politics"),
+  Category(icon: "#", title: "Technology"),
+  Category(icon: "#", title: "Startup"),
+  Category(icon: "#", title: "Entertainment"),
+  Category(icon: "#", title: "Miscellaneous"),
+  Category(icon: "#", title: "Hatke"),
+  Category(icon: "#", title: "Science"),
+  Category(icon: "#", title: "Autombile"),
+];
